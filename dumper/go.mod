7df8b43b08b2510f1c69e8f63f285bad48c5620a
João Fernandes <joaofnds@joaofnds.com>
2020-02-02 19:59:14 -0300

    Add Go HTTP server that dumps the raw request

---
+++ dumper/go.mod
@@ -0,0 +1,3 @@
+module github.com/joaofnds/traefik-playground
+
+go 1.13
---
+++ dumper/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable not set")
+	}
+
+	http.HandleFunc("/", rootHandler)
+
+	addr := ":" + port
+	server := http.Server{Addr: addr}
+
+	go (func() {
+		log.Fatal(server.ListenAndServe())
+	})()
+
+	log.Printf("Server up and running at %s\n", addr)
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	log.Printf("So long friend, goodbye...")
+
+	server.Shutdown(context.Background())
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+
+	b, err := httputil.DumpRequest(r, true)
+	if err == nil {
+		w.WriteHeader(200)
+		w.Write(b)
+	} else {
+		w.WriteHeader(500)
+		w.Write([]byte("failed to dump request"))
+	}
+}
